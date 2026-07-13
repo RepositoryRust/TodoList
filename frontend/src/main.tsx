4fc2a0761313496d1aca620d0e656ecabb00061a
thoriqtau <thoriqtau@gmail.com>
2026-07-13 23:17:06 +0700

    fix: logic flow

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import "./css/index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./pages/App";
 import LoginPage from "./pages/form/LoginPage";
+import RegisterPage from "./pages/form/RegisterPage";
+import NotFoundPage from "./pages/form/NotFoundPage";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
@@ -11,6 +13,8 @@ createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route element={<App />}>
           <Route path="/" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
